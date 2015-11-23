66cd41083c1c6451e19e6e797a3b9085c489d220
Jeremy Marquis <jeremy@jeremymarquis.com>
2015-11-22 21:03:13 -0500

    UI development

--- src/components/Composer.jsx
+++ src/components/Composer.jsx
@@ -8,13 +8,33 @@ import Image from "./Image";
 
 export default class Composer extends Component {
 
+	constructor (props) {
+		super(props);
+		this.state = {
+			message: "",
+			focused: false
+		};
+	}
+
 	render () {
 		return (
-			<div className="Composer">
+			<div className={ "Composer" + (this.state.focused ? " focused" : "") }>
 				<a><Image src={uploadIcon}/></a>
-				<textarea placeholder="Type your message..." autoComplete={false} spellCheck={true} autoCorrect={false}/>
+				<textarea placeholder="Type your message..." autoComplete={false} spellCheck={true} autoCorrect={false} onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleChange} value={this.state.message}/>
 			</div>
 		);
+	}
+
+	handleFocus = () => {
+		this.setState({ focused: true });
+	}
+
+	handleBlur = () => {
+		this.setState({ focused: false });
+	}
+
+	handleChange = (event) => {
+		this.setState({ message: event.target.value });
 	}
 
 }
